021994e075c469e5ab01dd6dccb2138b66b08d4e
Евгений <Sleepwalker17@mail.ru>
2018-06-13 00:36:41 +0300

    Updated UI

--- client/src/components/GoogleMap.vue
+++ client/src/components/GoogleMap.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    <div>
-      <h2>Map</h2>
-      <button @click="addMarkers">Add Markers</button>
-    </div>
+      <div class="footer">
+        Artists map
+      </div>
+    <!--<div>-->
+    <!--<h2>Map</h2>-->
+    <!--<button @click="addMarkers">Add Markers</button>-->
+    <!--</div>-->
+    <v-layout>
+      <label>User:</label>
+      <div class="input">
+        <input type="string" class="primary" v-model="user"/>
+      </div>
+      <div class="slidecontainer">
+        <input type="range" min="1" max="100" v-model="sliderValue" @input="changeWidth" class="slider" id="myRange">
+      </div>
+        <button class="btn btn-outline-primary"
+                @click="addMarkers"
+                target="_blank">Find
+        </button>
+    </v-layout>
     <br>
     <gmap-map
       :center="center"
@@ -45,7 +61,9 @@
             width: 0,
             height: -35
           }
-        }
+        },
+        sliderValue:50,
+        width: 50
       };
     },
 
@@ -89,7 +107,62 @@
           this.infoOpened = true;
           this.infoCurrentKey = key;
         }
+      },
+      changeWidth(){
+        this.width = this.sliderValue
       }
     }
   };
 </script>
+
+<style scoped>
+  .input {
+    margin-left: 12px;
+  }
+  .footer {
+    width: 100%;
+    height: 80px;
+    background-color: #303030;
+    margin: 0px;
+    padding: 0px;
+    color:white;
+    font-family: "Courier New";
+    font-size:30px;
+    text-align: center;
+  }
+
+  .slider {
+    -webkit-appearance: none;  /* Override default CSS styles */
+    appearance: none;
+    height: 10px; /* Specified height */
+    background: #d3d3d3; /* Grey background */
+    outline: none; /* Remove outline */
+    opacity: 0.7; /* Set transparency (for mouse-over effects on hover) */
+    -webkit-transition: .2s; /* 0.2 seconds transition on hover */
+    transition: opacity .2s;
+  }
+
+  /* Mouse-over effects */
+  .slider:hover {
+    opacity: 1; /* Fully shown on mouse-over */
+  }
+
+  /* The slider handle (use -webkit- (Chrome, Opera, Safari, Edge) and -moz- (Firefox) to override default look) */
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none; /* Override default look */
+    appearance: none;
+    width: 25px; /* Set a specific slider handle width */
+    height: 10px; /* Slider handle height */
+    background: #303030;
+    cursor: pointer; /* Cursor on hover */
+  }
+
+  .slider::-moz-range-thumb {
+    width: 25px; /* Set a specific slider handle width */
+    height: 10px; /* Slider handle height */
+    background: #4CAF50; /* Green background */
+    cursor: pointer; /* Cursor on hover */
+  }
+
+
+</style>
